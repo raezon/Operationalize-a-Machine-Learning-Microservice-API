e5170559bae2d737f0f81a582e207ed33bc039bb
Djebabla ammar <amardjebabla10@gmail.com>
2023-02-12 18:35:17 +0000

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=raezon/udacity-devops-nd-ml-api-project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag udacity-devops-nd-ml-api-project $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
