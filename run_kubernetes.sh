5a0b5f3a968747a20e85468df5dbddd96b009ca1
Djebabla ammar <amardjebabla10@gmail.com>
2023-02-12 18:55:34 +0000

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=babawale/udacity-devops-nd-ml-api-project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-devops-nd-ml-api-project\
+	--image=$dockerpath\
+	--port=80 --labels app=udacity-devops-nd-ml-api-project
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-devops-nd-ml-api-project 8000:80
