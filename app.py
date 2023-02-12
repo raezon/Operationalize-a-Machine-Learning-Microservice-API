5cf13ec2b680eb2d7502d96369136e6ed4f5af43
Djebabla ammar <amardjebabla10@gmail.com>
2023-02-12 19:47:31 +0000

    update app.py

--- app.py
+++ app.py
@@ -64,6 +64,7 @@ def predict():
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
     LOG.info(f"Prediction values: \n{prediction}")
+    return jsonify({'prediction': prediction})
 
 
 if __name__ == "__main__":
